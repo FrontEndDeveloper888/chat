61120bef8023f2710678a0d8d41be5ce5ad0f9df
FrontEndDeveloper888 <frontenddeveloper2008@gmail.com>
2023-08-07 03:40:19 +0500

    Finished

--- src/components/chat.js
+++ src/components/chat.js
@@ -1,12 +1,18 @@
-import React, {useState} from "react";
-import {Box, Typography, TextField, Button} from "@mui/material";
+import React, { useState, useRef, useEffect } from "react";
+import { Box, Typography, TextField,  IconButton, InputAdornment } from "@mui/material";
+import SendIcon from '@mui/icons-material/Send';
 
-const Chat = ({selectedContact, messages, setMessages}) => {
+const Chat = ({ selectedContact, messages, setMessages }) => {
     const [newMessage, setNewMessage] = useState("");
+    const messagesEndRef = useRef(null);
+
+    useEffect(() => {
+        scrollToBottom();
+    }, [messages]);
 
     const handleSendMessage = () => {
         if (newMessage.trim() !== "") {
-            const newMessages = [...messages, {sender: "me", text: newMessage}];
+            const newMessages = [...messages, { sender: "me", text: newMessage }];
             setMessages(newMessages);
             setNewMessage("");
         }
@@ -27,49 +33,108 @@ const Chat = ({selectedContact, messages, setMessages}) => {
         return lines;
     };
 
-    return (<Box sx={{width: "80%", bg: "white", padding: "16px", background: "#0E1621"}}>
-        {selectedContact ? (<>
-            <Box sx={{marginBottom: "36px"}}>
-                {messages.map((message, index) => (<Box key={index}
-                                                        sx={{
-                                                            textAlign: message.sender === "me" ? "right" : "left",
-                                                            marginBottom: "8px"
-                                                        }}>
-                    <Typography
+    const scrollToBottom = () => {
+        if (messagesEndRef.current) {
+            messagesEndRef.current.scrollIntoView({ behavior: "smooth", block: "end" });
+        }
+    };
+
+    return (
+        <Box sx={{ width: "80%", padding: "15px", background: "#0E1621" }}>
+            {selectedContact ? (
+                <>
+                    <Box
                         sx={{
-                            backgroundColor: message.text.length > 100 ? "red" : "green",
-                            padding: "8px",
-                            minWidth: "200px",
-                            marginBottom: "8px",
-                            borderRadius: "8px",
+                            marginBottom: "36px",
+                            maxHeight: "calc(100vh - 120px)",
+                            overflowY: "auto",
+                        }}
+                    >
+                        {messages.map((message, index) => (
+                            <Box
+                                key={index}
+                                sx={{
+                                    textAlign: message.sender === "me" ? "right" : "left",
+                                    marginBottom: "8px",
+                                }}
+                            >
+                                <Typography
+                                    sx={{
+                                        background: "#2B5279",
+                                        padding: "8px",
+                                        mx: "10px",
+                                        minWidth: "auto",
+                                        marginBottom: "5px",
+                                        borderRadius: "8px",
+                                        color: "white",
+                                        display: "inline-block",
+                                        textAlign: "right",
+                                    }}
+                                >
+                                    {splitMessageText(message.text).map((line, i) => (
+                                        <React.Fragment key={i}>
+                                            {line}
+                                            <br />
+                                        </React.Fragment>
+                                    ))}
+                                </Typography>
+                            </Box>
+                        ))}
+                        <div ref={messagesEndRef} />
+                    </Box>
+                    <Box
+                        sx={{
+                            position: "fixed",
+                            bottom: 5,
+                            width: "78%",
+                            display: "flex",
+                            alignItems: "center",
+                            padding: "5px",
                             color: "white",
                         }}
                     >
-                        {splitMessageText(message.text).map((line, i) => (<React.Fragment key={i}>
-                            {line}
-                            <br/>
-                        </React.Fragment>))}
-                    </Typography>
-                </Box>))}
-            </Box>
-
-            <Box sx={{display: "flex", alignItems: "center", justifyContent: "flex-end"}}>
-                <TextField
-                    value={newMessage}
-                    onChange={(e) => setNewMessage(e.target.value)}
-                    label="Type your message"
-                    variant="outlined"
-                    sx={{flex: 1, marginRight: "16px"}}
-                    onKeyDown={handleKeyDown}
-                />
-                <Button onClick={handleSendMessage} variant="contained" color="primary">
-                    Send
-                </Button>
-            </Box>
-        </>) : (<Typography variant="h6" sx={{textAlign: "center", marginTop: "30%"}}>
-            Select a contact to start chatting
-        </Typography>)}
-    </Box>);
+                        <TextField
+                            value={newMessage}
+                            onChange={(e) => setNewMessage(e.target.value)}
+                            placeholder="Type your message"
+                            variant="outlined"
+                            sx={{
+                                flex: 1,
+                                "& .MuiOutlinedInput-input": {
+                                    color: "white",
+                                },
+                                "& .MuiOutlinedInput-root": {
+                                    "& fieldset": {
+                                        border: "1px solid grey",
+                                    },
+                                    "&:hover fieldset": {
+                                        border: "1px solid grey",
+                                    },
+                                    "&.Mui-focused fieldset": {
+                                        border: "1px solid grey",
+                                    },
+                                },
+                            }}
+                            onKeyDown={handleKeyDown}
+                            InputProps={{
+                                endAdornment: (
+                                    <InputAdornment position="end">
+                                        <IconButton onClick={handleSendMessage} color="primary">
+                                            <SendIcon />
+                                        </IconButton>
+                                    </InputAdornment>
+                                )
+                            }}
+                        />
+                    </Box>
+                </>
+            ) : (
+                <Typography variant="h6" sx={{ textAlign: "center", marginTop: "30%", color: "lightgrey" }}>
+                    Select a contact to start chatting
+                </Typography>
+            )}
+        </Box>
+    );
 };
 
 export default Chat;
--- src/components/contact.js
+++ src/components/contact.js
@@ -1,8 +1,17 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import {
-    Box, List, ListItem, ListItemText, TextField, Button, Avatar, Modal, IconButton, InputAdornment
+    Box,
+    List,
+    ListItem,
+    ListItemText,
+    TextField,
+    Button,
+    Avatar,
+    Modal,
+    InputAdornment,
 } from "@mui/material";
-const Contacts = ({contacts, onContactSelect, selectedContact, onAddContact}) => {
+
+const Contacts = ({ contacts, onContactSelect, selectedContact, onAddContact }) => {
     const [newContactName, setNewContactName] = useState("");
     const [newContactPhone, setNewContactPhone] = useState("");
     const [searchQuery, setSearchQuery] = useState("");
@@ -18,7 +27,9 @@ const Contacts = ({contacts, onContactSelect, selectedContact, onAddContact}) =>
             let phoneNumber = newContactPhone.trim().replace(/[^0-9]/g, "");
             phoneNumber = `+998 ${phoneNumber.slice(0, 2)} ${phoneNumber.slice(2, 5)}-${phoneNumber.slice(5, 7)}-${phoneNumber.slice(7, 9)}`;
             const newContact = {
-                id: contacts.length + 1, name: newContactName, phone: phoneNumber,
+                id: contacts.length + 1,
+                name: newContactName,
+                phone: phoneNumber,
             };
             onAddContact(newContact);
             setNewContactName("");
@@ -29,119 +40,129 @@ const Contacts = ({contacts, onContactSelect, selectedContact, onAddContact}) =>
 
     const handleSearchContacts = (query) => {
         setSearchQuery(query);
-        const filtered = contacts.filter((contact) => contact.name.toLowerCase().includes(query.toLowerCase()));
+        const filtered = contacts.filter((contact) =>
+            contact.name.toLowerCase().includes(query.toLowerCase())
+        );
         setFilteredContacts(filtered);
     };
 
-    return (<Box sx={{width: "20%",}}>
-        <Box sx={{padding: "10px", alignItems: "center", justifyContent: "space-between"}}>
-            <TextField
-                value={searchQuery}
-                onChange={(e) => handleSearchContacts(e.target.value)}
-                placeholder="Search contact"
-                variant="outlined"
-                color={"primary"}
-                fullWidth
-                sx={{
-                    backgroundColor: "#232F3D", // Set the background color to red
-                    border: "none", borderRadius: "30px", color: "white", // Set the text color to white
-                    "& .MuiOutlinedInput-notchedOutline": {
-                        border: "none", color: "white",// Hide the outline border
-                    }, "&:hover .MuiOutlinedInput-notchedOutline": {
-                        border: "none", // Hide the outline border on hover
-                    }, "& input::placeholder": {
-                        color: "#557A8C", margin: "10px"// Set the placeholder color to grey
-                    },
-                }}
-            />
-
-        </Box>
-        <Box sx={{maxHeight: "calc(95vh - 100px)", overflowY: "auto"}}>
-            <List>
-                {filteredContacts.map((contact) => (<ListItem
-                    key={contact.id}
-                    button
-                    onClick={() => onContactSelect(contact)}
+    return (
+        <Box sx={{ width: "20%" }}>
+            <Box sx={{ padding: "10px", alignItems: "center", justifyContent: "space-between" }}>
+                <TextField
+                    value={searchQuery}
+                    onChange={(e) => handleSearchContacts(e.target.value)}
+                    placeholder="Search contact"
+                    variant="outlined"
+                    color="primary"
+                    fullWidth
                     sx={{
-                        backgroundColor: selectedContact && selectedContact.id === contact.id ? "#2B5178" : "#17212B",
-                        display: "flex",
-                        alignItems: "center",
-                        ":hover": {
-                            backgroundColor: "#212B35", // Set background to transparent on hover
+                        backgroundColor: "#232F3D",
+                        border: "none",
+                        borderRadius: "30px",
+                        color: "white",
+                        "& .MuiOutlinedInput-notchedOutline": {
+                            border: "none",
+                            color: "white",
+                        },
+                        "&:hover .MuiOutlinedInput-notchedOutline": {
+                            border: "none",
+                        },
+                        "& input::placeholder": {
+                            color: "#557A8C",
+                            margin: "10px",
                         },
                     }}
-                >
-                    {contact.avatarUrl ? (
-                        <Avatar sx={{marginRight: "16px"}} alt={contact.name} src={contact.avatarUrl}/>) : (
-                        <Avatar sx={{marginRight: "16px"}}>{contact.name.charAt(0)}</Avatar>
-
-                    )}
-                    <ListItemText
-                        primaryTypographyProps={{color: "white"}} // Set the text color to white
-                        primary={contact.name}
-                        secondaryTypographyProps={{color: "lightgrey"}}
-                        secondary={`Mobile: ${contact.phone}`} // Display "Mobile: " followed by the phone number
-                    />
-                </ListItem>))}
-            </List>
-        </Box>
-        <Box sx={{padding: "16px"}}> {/* Container for the fixed "+ button" */}
-            <Button onClick={() => setIsModalOpen(true)} variant="contained" color="primary" fullWidth>
-                Add Contact
-            </Button>
-        </Box>
-        <Modal open={isModalOpen} onClose={() => setIsModalOpen(false)}>
-            <Box
-                sx={{
-                    position: "absolute",
-                    top: "50%",
-                    left: "50%",
-                    transform: "translate(-50%, -50%)",
-                    backgroundColor: "lightgrey",
-                    p: 4,
-                    minWidth: "300px",
-                    borderRadius: "8px",
-                    boxShadow: "0 2px 5px rgba(0, 0, 0, 1)", // Add shadow
-                    display: "flex",
-                    flexDirection: "column",
-                    alignItems: "center",
-                }}
-            >
-                <TextField
-                    value={newContactName}
-                    onChange={(e) => setNewContactName(e.target.value)}
-                    label="Enter new contact name"
-                    variant="outlined"
-                    fullWidth
-                    sx={{ marginBottom: "16px" }}
                 />
-                <TextField
-                    value={newContactPhone}
-                    onChange={(e) => {
-                        let phoneNumber = e.target.value.replace(/[^0-9+]/g, "");
-                        if (phoneNumber.length > 3) {
-                            phoneNumber =
-                                phoneNumber.slice(0, 4) +
-                                phoneNumber.slice(4, 13).replace(/[^0-9]/g, "").slice(0, 9);
-                        }
-                        setNewContactPhone(phoneNumber);
-                    }}
-                    label="Enter new contact phone"
-                    variant="outlined"
-                    fullWidth
-                    InputProps={{
-                        startAdornment: <InputAdornment position="start">+998</InputAdornment>,
-                    }}
-                    sx={{ marginBottom: "16px" }}
-                />
-
-                <Button onClick={handleAddContact} variant="contained" color="primary" fullWidth>
+            </Box>
+            <Box sx={{ maxHeight: "calc(100vh - 150px)", overflowY: "auto" }}>
+                <List>
+                    {filteredContacts.map((contact) => (
+                        <ListItem
+                            key={contact.id}
+                            button
+                            onClick={() => onContactSelect(contact)}
+                            sx={{
+                                backgroundColor: selectedContact && selectedContact.id === contact.id ? "#2B5178" : "#17212B",
+                                display: "flex",
+                                alignItems: "center",
+                                ":hover": {
+                                    backgroundColor: "#212B35",
+                                },
+                            }}
+                        >
+                            {contact.avatarUrl ? (
+                                <Avatar sx={{ marginRight: "16px" }} alt={contact.name} src={contact.avatarUrl} />
+                            ) : (
+                                <Avatar sx={{ marginRight: "16px" }}>{contact.name.charAt(0)}</Avatar>
+                            )}
+                            <ListItemText
+                                primaryTypographyProps={{ color: "white" }}
+                                primary={contact.name}
+                                secondaryTypographyProps={{ color: "lightgrey" }}
+                                secondary={`Mobile: ${contact.phone}`}
+                            />
+                        </ListItem>
+                    ))}
+                </List>
+            </Box>
+            <Box sx={{ padding: "16px" }}>
+                <Button onClick={() => setIsModalOpen(true)} variant="contained" color="primary" fullWidth>
                     Add Contact
                 </Button>
             </Box>
-        </Modal>
+            <Modal open={isModalOpen} onClose={() => setIsModalOpen(false)}>
+                <Box
+                    sx={{
+                        position: "absolute",
+                        top: "50%",
+                        left: "50%",
+                        transform: "translate(-50%, -50%)",
+                        backgroundColor: "white",
+                        p: 4,
+                        minWidth: "300px",
+                        borderRadius: "8px",
+                        boxShadow: "0 2px 5px rgba(0, 0, 0, 0.2)",
+                        display: "flex",
+                        flexDirection: "column",
+                        alignItems: "center",
+                    }}
+                >
+                    <TextField
+                        value={newContactName}
+                        onChange={(e) => setNewContactName(e.target.value)}
+                        label="Enter new contact name"
+                        variant="outlined"
+                        fullWidth
+                        sx={{ marginBottom: "16px" }}
+                    />
+                    <TextField
+                        value={newContactPhone}
+                        onChange={(e) => {
+                            let phoneNumber = e.target.value.replace(/[^0-9+]/g, "");
+                            if (phoneNumber.length > 3) {
+                                phoneNumber =
+                                    phoneNumber.slice(0, 4) +
+                                    phoneNumber.slice(4, 13).replace(/[^0-9]/g, "").slice(0, 9);
+                            }
+                            setNewContactPhone(phoneNumber);
+                        }}
+                        label="Enter new contact phone"
+                        variant="outlined"
+                        fullWidth
+                        InputProps={{
+                            startAdornment: <InputAdornment position="start">+998</InputAdornment>,
+                        }}
+                        sx={{ marginBottom: "16px" }}
+                    />
 
-    </Box>);
+                    <Button onClick={handleAddContact} variant="contained" color="primary" fullWidth>
+                        Add Contact
+                    </Button>
+                </Box>
+            </Modal>
+        </Box>
+    );
 };
 
 export default Contacts;
